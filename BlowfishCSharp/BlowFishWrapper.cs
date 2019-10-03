985b50a8127228023222fb13a70672048c87c21d
hkirschke <33073460+hkirschke@users.noreply.github.com>
2019-10-03 20:28:31 -0300

    Create Project

---
+++ BlowfishCSharp/BlowFishWrapper.cs
@@ -0,0 +1,29 @@
+﻿
+using crypto;
+namespace BlowfishCSharp.Wrapper
+{
+    public static class BlowFishWrapper
+    {
+        public static string Encrypt(string value)
+        {
+            var result = GenerateKeyIV();
+            return Security.Encrypt(value, result[0], result[1]);
+        }
+        public static string Decrypt(string value)
+        {
+            var result = GenerateKeyIV();
+            return Security.Decrypt(value, result[0], result[1]);
+        }
+        /// <summary>
+        /// Retorna array com dois valores Key e IV
+        /// </summary>
+        /// <returns></returns>
+        private static string[] GenerateKeyIV()
+        {
+            var array = new string[2];
+            array[0] = Security.GenerateText(32);
+            array[1] = Security.GenerateText(16);
+            return array;
+        }
+    }
+}
