826567de7c2110c06e4242010135d2c7f8388298
hkirschke <henriquek.a@hotmail.com>
2019-10-04 15:02:08 -0300

    Update
    Create const to KEY and IV ,  Class Teste

--- BlowfishCSharp/Program.cs
+++ BlowfishCSharp/Program.cs
@@ -7,11 +7,11 @@ using BlowfishCSharp.Wrapper;
 
 namespace BlowfishCSharp
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-
-        }
+     
     }
+  }
 }
---
+++ BlowfishCSharp/Teste.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BlowfishCSharp.Wrapper;
+
+namespace BlowfishCSharp
+{
+  public static class Teste
+  {
+    public static string RunTest(string texto)
+    {
+      var textoCript = BlowFishWrapper.Encrypt(texto);
+      var textoDescrip = BlowFishWrapper.Decrypt(textoCript);
+      return textoDescrip;
+    }
+  }
+}
