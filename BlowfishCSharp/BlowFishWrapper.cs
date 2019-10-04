b0e0b94420b54e94621483a5158ca4e79ba32d55
hkirschke <henriquek.a@hotmail.com>
2019-10-04 15:08:53 -0300

    Update Wrapper
    Defined consts

--- BlowfishCSharp/BlowFishWrapper.cs
+++ BlowfishCSharp/BlowFishWrapper.cs
@@ -1,29 +1,30 @@
-﻿
-using crypto;
+﻿using crypto;
+
 namespace BlowfishCSharp.Wrapper
 {
-    public static class BlowFishWrapper
+  public static class BlowFishWrapper
+  {
+    private const string KEY = "v68am2Vb5NQoAI9sNrKtQxpbdYrjX4Qixhg315OJdjo=";
+    private const string IV = "tcONmecWWfar1dT1H/1WoA==";
+
+    public static string Encrypt(string value)
     {
-        public static string Encrypt(string value)
-        {
-            var result = GenerateKeyIV();
-            return Security.Encrypt(value, result[0], result[1]);
-        }
-        public static string Decrypt(string value)
-        {
-            var result = GenerateKeyIV();
-            return Security.Decrypt(value, result[0], result[1]);
-        }
-        /// <summary>
-        /// Retorna array com dois valores Key e IV
-        /// </summary>
-        /// <returns></returns>
-        private static string[] GenerateKeyIV()
-        {
-            var array = new string[2];
-            array[0] = Security.GenerateText(32);
-            array[1] = Security.GenerateText(16);
-            return array;
-        }
+      return Security.Encrypt(value, KEY, IV);
     }
+    public static string Decrypt(string value)
+    {
+      return Security.Decrypt(value, KEY, IV);
+    }
+    /// <summary>
+    /// Retorna array com dois valores Key e IV
+    /// </summary>
+    /// <returns></returns>
+    public static string[] GenerateKeyIV()
+    {
+      var array = new string[2];
+      array[0] = Security.GenerateText(32);
+      array[1] = Security.GenerateText(16);
+      return array;
+    }
+  }
 }
